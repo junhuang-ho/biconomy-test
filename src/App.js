af4bc1a18579b8fb94383d07ba71adf40f4c13b1
turmetirc-blend <junhuangho27@gmail.com>
2022-07-14 10:44:06 +0800

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,91 @@ const MUMBAI_CFAV1_ADDRESS = "0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873"
 const MUMBAI_fDAIx_ADDRESS = "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f" // token
 const OPERATOR_ADDRESS = "0x614539062F7205049917e03ec4C86FF808F083cb" // any EOA address that is not the sender
 
+const MUMBAI_BICONOMY_FORWARDER_ADDRESS = "0x9399BB24DBB5C4b782C70c2969F58716Ebbd6a3b"
+// const MUMBAI_PART_OF_BICONOMY_FORWARDER_ABI = [{ "inputs": [{ "components": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "address", "name": "token", "type": "address" }, { "internalType": "uint256", "name": "txGas", "type": "uint256" }, { "internalType": "uint256", "name": "tokenGasPrice", "type": "uint256" }, { "internalType": "uint256", "name": "batchId", "type": "uint256" }, { "internalType": "uint256", "name": "batchNonce", "type": "uint256" }, { "internalType": "uint256", "name": "deadline", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "internalType": "struct ERC20ForwardRequestTypes.ERC20ForwardRequest", "name": "req", "type": "tuple" }, { "internalType": "bytes", "name": "sig", "type": "bytes" }], "name": "executePersonalSign", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }, { "internalType": "bytes", "name": "ret", "type": "bytes" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "uint256", "name": "batchId", "type": "uint256" }], "name": "getNonce", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "components": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "address", "name": "token", "type": "address" }, { "internalType": "uint256", "name": "txGas", "type": "uint256" }, { "internalType": "uint256", "name": "tokenGasPrice", "type": "uint256" }, { "internalType": "uint256", "name": "batchId", "type": "uint256" }, { "internalType": "uint256", "name": "batchNonce", "type": "uint256" }, { "internalType": "uint256", "name": "deadline", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "internalType": "struct ERC20ForwardRequestTypes.ERC20ForwardRequest", "name": "req", "type": "tuple" }, { "internalType": "bytes", "name": "sig", "type": "bytes" }], "name": "verifyPersonalSign", "outputs": [], "stateMutability": "view", "type": "function" }]
+const MUMBAI_BICONOMY_FORWARDER_ABI = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "domainSeparator", "type": "bytes32" }, { "indexed": false, "internalType": "bytes", "name": "domainValue", "type": "bytes" }], "name": "DomainRegistered", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "userAddress", "type": "address" }, { "indexed": true, "internalType": "address", "name": "relayerAddress", "type": "address" }, { "indexed": true, "internalType": "bytes", "name": "functionSignature", "type": "bytes" }], "name": "MetaTransactionExecuted", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "inputs": [], "name": "EIP712_DOMAIN_TYPE", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "REQUEST_TYPEHASH", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "name": "domains", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "components": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "address", "name": "token", "type": "address" }, { "internalType": "uint256", "name": "txGas", "type": "uint256" }, { "internalType": "uint256", "name": "tokenGasPrice", "type": "uint256" }, { "internalType": "uint256", "name": "batchId", "type": "uint256" }, { "internalType": "uint256", "name": "batchNonce", "type": "uint256" }, { "internalType": "uint256", "name": "deadline", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "internalType": "struct ForwardRequestTypes.ERC20ForwardRequest", "name": "req", "type": "tuple" }, { "internalType": "bytes32", "name": "domainSeparator", "type": "bytes32" }, { "internalType": "bytes", "name": "sig", "type": "bytes" }], "name": "executeEIP712", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }, { "internalType": "bytes", "name": "ret", "type": "bytes" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "components": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "address", "name": "token", "type": "address" }, { "internalType": "uint256", "name": "txGas", "type": "uint256" }, { "internalType": "uint256", "name": "tokenGasPrice", "type": "uint256" }, { "internalType": "uint256", "name": "batchId", "type": "uint256" }, { "internalType": "uint256", "name": "batchNonce", "type": "uint256" }, { "internalType": "uint256", "name": "deadline", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "internalType": "struct ForwardRequestTypes.ERC20ForwardRequest", "name": "req", "type": "tuple" }, { "internalType": "bytes", "name": "sig", "type": "bytes" }], "name": "executePersonalSign", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }, { "internalType": "bytes", "name": "ret", "type": "bytes" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "uint256", "name": "batchId", "type": "uint256" }], "name": "getNonce", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "name", "type": "string" }, { "internalType": "string", "name": "version", "type": "string" }], "name": "registerDomainSeparator", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "components": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "address", "name": "token", "type": "address" }, { "internalType": "uint256", "name": "txGas", "type": "uint256" }, { "internalType": "uint256", "name": "tokenGasPrice", "type": "uint256" }, { "internalType": "uint256", "name": "batchId", "type": "uint256" }, { "internalType": "uint256", "name": "batchNonce", "type": "uint256" }, { "internalType": "uint256", "name": "deadline", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "internalType": "struct ForwardRequestTypes.ERC20ForwardRequest", "name": "req", "type": "tuple" }, { "internalType": "bytes32", "name": "domainSeparator", "type": "bytes32" }, { "internalType": "bytes", "name": "sig", "type": "bytes" }], "name": "verifyEIP712", "outputs": [], "stateMutability": "view", "type": "function" }, { "inputs": [{ "components": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "address", "name": "token", "type": "address" }, { "internalType": "uint256", "name": "txGas", "type": "uint256" }, { "internalType": "uint256", "name": "tokenGasPrice", "type": "uint256" }, { "internalType": "uint256", "name": "batchId", "type": "uint256" }, { "internalType": "uint256", "name": "batchNonce", "type": "uint256" }, { "internalType": "uint256", "name": "deadline", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "internalType": "struct ForwardRequestTypes.ERC20ForwardRequest", "name": "req", "type": "tuple" }, { "internalType": "bytes", "name": "sig", "type": "bytes" }], "name": "verifyPersonalSign", "outputs": [], "stateMutability": "view", "type": "function" }]
+const MUMBAI_HASHER_ADDRESS = "0xabF9893851D2aA7078575274848307247c6F34c7"
+const MUMBAI_HASHER_ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_token",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_txGas",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_tokenGasPrice",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_batchId",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_batchNonce",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_deadline",
+                "type": "uint256"
+            },
+            {
+                "internalType": "bytes",
+                "name": "_data",
+                "type": "bytes"
+            }
+        ],
+        "name": "getMessageHash",
+        "outputs": [
+            {
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes",
+                "name": "_data",
+                "type": "bytes"
+            }
+        ],
+        "name": "hashWithKeccak256",
+        "outputs": [
+            {
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    }
+]
+
 function App()
 {
 
@@ -25,6 +110,7 @@ function App()
     const [sfInterface, setSFInterface] = useState(null)
     const [cfav1Interface, setCFAV1Interface] = useState(null)
     const [cfav1Contract, setCFAV1Contract] = useState(null)
+    const [sfContract, setSFContract] = useState(null)
 
     useEffect(() =>
     {
@@ -55,11 +141,17 @@ function App()
                 setSFInterface(sfInterface)
                 setCFAV1Interface(cfav1Interface)
 
+                const sfContract = new ethers.Contract(
+                    MUMBAI_SF_HOST_ADDRESS,
+                    ISuperfluid.abi,
+                    walletSigner,
+                )
                 const cfav1Contract = new ethers.Contract(
                     MUMBAI_CFAV1_ADDRESS,
                     IConstantFlowAgreementV1.abi,
                     walletSigner,
                 )
+                setSFContract(sfContract)
                 setCFAV1Contract(cfav1Contract)
 
                 setBiconomyProvider(biconomy.getEthersProvider())
@@ -176,7 +268,7 @@ function App()
             [
                 [
                     201, // operation type, sf specific
-                    MUMBAI_SF_HOST_ADDRESS,
+                    MUMBAI_CFAV1_ADDRESS,
                     callData,
                 ],
             ]
@@ -212,24 +304,114 @@ function App()
             [
                 [
                     201, // operation type, sf specific
-                    MUMBAI_SF_HOST_ADDRESS,
+                    MUMBAI_CFAV1_ADDRESS,
                     callData,
                 ],
             ]
         ])
 
         // Suggestion from the SF Team
-        const biconomyForwarderInterface = new ethers.utils.Interface(/** BiconomyForwarderABI ???? */); // mumbai Biconomy Forwarder address is: 0x9399BB24DBB5C4b782C70c2969F58716Ebbd6a3b
-        const hash = "0x" // <how to get> // <------ ????
+        const biconomyForwarderInterface = new ethers.utils.Interface(MUMBAI_BICONOMY_FORWARDER_ABI);
+        const biconomyForwarderContract = new ethers.Contract(
+            MUMBAI_BICONOMY_FORWARDER_ADDRESS,
+            MUMBAI_BICONOMY_FORWARDER_ABI,
+            signer,
+        )
+
+        const hasherContract = new ethers.Contract(
+            MUMBAI_HASHER_ADDRESS,
+            MUMBAI_HASHER_ABI,
+            signer,
+        )
+
+        // // GAS ESTIMATES
+        // console.log("YO")
+        // const estimatedGasUpdateFlow = await cfav1Contract.estimateGas.updateFlowOperatorPermissions(
+        //     MUMBAI_fDAIx_ADDRESS,
+        //     OPERATOR_ADDRESS,
+        //     "4", // grants operator the delete permission (example only)
+        //     "0", // flow allowance is 0 for delete permission
+        //     [], // empty
+        // )
+        // const estimatedGasRevokeFlow = cfav1Contract.estimateGas.revokeFlowOperatorWithFullControl(
+        //     MUMBAI_fDAIx_ADDRESS,
+        //     OPERATOR_ADDRESS,
+        //     [], // empty
+        // )
+        // const estimatedGasCallAgreement = sfContract.estimateGas.callAgreement(
+        //     MUMBAI_CFAV1_ADDRESS,
+        //     data,
+        //     "0x", // userData is empty
+        // )
+        // const estimatedGasForwardCall = await sfContract.estimateGas.forwardBatchCall(
+        //     [
+        //         [
+        //             201, // operation type, sf specific
+        //             MUMBAI_SF_HOST_ADDRESS,
+        //             callData,
+        //         ],
+        //     ]
+        // )
+        // console.log("GAS ESTIMATES")
+        // console.log(estimatedGasUpdateFlow)
+        // console.log(estimatedGasRevokeFlow)
+        // console.log(estimatedGasCallAgreement)
+        // console.log(estimatedGasForwardCall)
+
+        const token = ethers.constants.AddressZero // is only used if you want to collect gas fees in an ERC20
+        const txGas = "9000000000000000" // is how much gas (in wei) you're going to forward to the Host contract
+        const tokenGasPrice = "0" // is only used if you want to collect fees in an ERC20
+        const batchId = "0" // can be always zero
+        const batchNonce = (await biconomyForwarderContract.getNonce(selectedAddress, batchId)).toString()
+
+        const now = Math.floor(new Date().getTime() / 1000.0)
+        const minutesFromNow = 5
+        const deadline = (now + (minutesFromNow * 60)).toString()
+
+        const hashedData = await hasherContract.hashWithKeccak256(forwardCallData)
+
+        const hash = await hasherContract.getMessageHash(
+            selectedAddress,
+            MUMBAI_SF_HOST_ADDRESS,
+            token,
+            txGas,
+            tokenGasPrice,
+            batchId,
+            batchNonce,
+            deadline,
+            hashedData,
+        )
         const signature = await signer.signMessage(ethers.utils.arrayify(hash))
+
         const biconomyForwarderData = biconomyForwarderInterface.encodeFunctionData("executePersonalSign", [
             [
-                "", // <ERC20ForwardRequest params> // <------ ???? https://github.com/bcnmy/mexa/blob/2f4957cd2253ef7cdfbee09c0844c9b71492034e/contracts/6/forwarder/ERC20ForwardRequestTypes.sol#L21
-                "",
-                forwardCallData, // the "data" param in ERC20ForwardRequest
+                selectedAddress,
+                MUMBAI_SF_HOST_ADDRESS,
+                token,
+                txGas,
+                tokenGasPrice,
+                batchId,
+                batchNonce,
+                deadline,
+                hashedData,
             ],
             signature
         ])
+
+        // const biconomyForwarderData = biconomyForwarderInterface.encodeFunctionData("verifyPersonalSign", [
+        //     [
+        //         selectedAddress,
+        //         MUMBAI_SF_HOST_ADDRESS,
+        //         token,
+        //         txGas,
+        //         tokenGasPrice,
+        //         batchId,
+        //         batchNonce,
+        //         deadline,
+        //         hashedData,
+        //     ],
+        //     signature
+        // ])
 
         const txParams = {
             data: biconomyForwarderData,
@@ -238,7 +420,15 @@ function App()
         };
 
         // Switch provider from normal to Biconomy ******
-        const tx = await biconomyProvider.send("eth_sendTransaction", [txParams])
+        let tx
+        try
+        {
+            tx = await biconomyProvider.send("eth_sendTransaction", [txParams])
+        } catch (error)
+        {
+            console.error(error)
+        }
+
         biconomyProvider.once(tx, (transaction) =>
         { // Emitted when the transaction has been mined
             setRefresh(!refresh)
@@ -341,6 +531,13 @@ function App()
                     <p>2. What is the full ABI for it? (mumbai)</p>
                     <p>3. In the ERC20ForwardRequest params, what is the best values for `txGas`, `tokenGasPrice`, `batchId`, `batchNonce` and `deadline`? Are these params normally done for us on the Biconomy side of things? How do I set them?</p>
                     <p>Also, I am still confused on the proper workflow for everything here, general explanations/tutorials/guides would help</p>
+
+                    <button
+                        onClick={ async () =>
+                        {
+                            await runBiconomyFlowWithSFSuggestions(grantPermission)
+                        } }
+                    >dummy</button>
                 </div>
             ) : (
                 <div>loading...</div>
